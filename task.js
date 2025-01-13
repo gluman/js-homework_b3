b19d089f61a684f8a9c72a8b91f636fcf7c2f8ab
Andrey Glumov <gluman@yandex.ru>
2025-01-13 23:44:18 +0300

    some fix

--- task.js
+++ task.js
@@ -5,14 +5,12 @@ const tabs = document.querySelectorAll('.tab');
 tabs.forEach((tab, i) => {
 
     tab.addEventListener('click', () => {
-      
+
         tabs.forEach(tab => {
             tab.classList.remove('tab_active')
         });
         tab.classList.add('tab_active')
-        // Удаляем класс 'tab__content_active' у всех разделов
         tabs_content.forEach(content => content.classList.remove('tab__content_active'));
-        // Добавляем класс 'tab__content_active' к нужному разделу
         tabs_content[i].classList.add('tab__content_active');
 
 
